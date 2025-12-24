91039a5f940d47f6ae94830ca760fe822bf4444f
Philoso4er <141080908+Philoso4er@users.noreply.github.com>
2025-12-24 03:56:00 +0100

    Render Admin component based on pathname

--- index.tsx
+++ index.tsx
@@ -1,16 +1,12 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Admin from './Admin';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const isAdmin = window.location.pathname === '/admin';
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </React.StrictMode>
 );
