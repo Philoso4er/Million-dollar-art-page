47d8131d135ab228bb45a7b1766d8d05cc3b479c
Philoso4er <141080908+Philoso4er@users.noreply.github.com>
2025-12-24 04:34:17 +0100

    Update Admin import path to src directory

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Admin from './Admin';
+import Admin from './src/Admin';
 
 const isAdmin = window.location.pathname === '/admin';
 
