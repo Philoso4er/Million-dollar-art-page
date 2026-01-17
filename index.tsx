b89e7d0b7da90b485424f9d80ed2c7902703ee96
Philoso4er <141080908+Philoso4er@users.noreply.github.com>
2026-01-17 02:43:56 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// Admin is now handled from /admin folder through routing, not imports.
-// DO NOT IMPORT ADMIN HERE.
-
-const root = ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
