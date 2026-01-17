17d2842e4514b0ddafa55a3a2cd02d600cee2d42
Philoso4er <141080908+Philoso4er@users.noreply.github.com>
2026-01-17 02:48:28 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
-  readonly VITE_FLW_PUBLIC_KEY?: string;
+  readonly VITE_FLW_PUBLIC_KEY: string;
+  readonly VITE_ADMIN_PASSWORD: string;
 }
 
 interface ImportMeta {
